6cce81b2f310c54180229a26bb5ab14b98475e0f
Oleksandr.bayda <oleksandr.bayda@cantire.com>
2023-05-18 20:19:17 +0300

    add maven to POJO

--- src/test/java/api/ContractTest.java
+++ src/test/java/api/ContractTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import setup.BaseTest;
 
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContractTest extends BaseTest {
 
@@ -12,8 +13,11 @@ public class ContractTest extends BaseTest {
     public void restGet() {
         Response response = controller.getSchema(1);
 
-        response.then()
-                .assertThat().body(matchesJsonSchemaInClasspath("product.json"));
+        assertThat(
+                "Response does not match the expected schema",
+                response.getBody().asString(),
+                matchesJsonSchemaInClasspath("product.json")
+        );
     }
 }
 
